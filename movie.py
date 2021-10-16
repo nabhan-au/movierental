457fa97d75e4e1101e11a682c30c90afb4212ff1
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-10-17 01:08:17 +0700

    add enum to movie

--- movie.py
+++ movie.py
@@ -1,14 +1,37 @@
+from enum import Enum
+
+
+class PriceCode(Enum):
+	"""An enumeration for different kinds of movies and their behavior"""
+	new_release = { "price": lambda days: 3.0*days,
+					"frp": lambda days: days
+				  }
+	regular = { "price": lambda days: 2 if days<=2 else 2 + 1.5*(days-2),
+				"frp": lambda days: 1
+			 }
+	childrens = {"price": lambda days: 1.5 if days<=3 else 1.5 + 1.5*(days-3),
+				 "frp": lambda days: 1
+			    }
+
+	def price(self, days: int) -> float:
+            "Return the rental price for a given number of days"""
+            pricing = self.value["price"]    # the enum member's price formula
+            return pricing(days)
+
+	def point(self, days: int) -> int:
+		"Retrun the point for a given number of days"
+		point = self.value["frp"]
+		return point(days)
+
 class Movie:
 	"""
 	A movie available for rent.
 	"""
-	# The types of movies (price_code). 
-	REGULAR = 0
-	NEW_RELEASE = 1
-	CHILDRENS = 2
 	
 	def __init__(self, title, price_code):
-		# Initialize a new movie. 
+		# Initialize a new movie.
+		if not isinstance(price_code, PriceCode):
+			raise TypeError("movie must be Price code")
 		self.title = title
 		self.price_code = price_code
 
