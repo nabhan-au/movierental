12209f3d710e19c380ea5f5e35ba8a12a16e7bf4
Nabhan Suwanachote <nabhan.au@gmail.com>
2021-10-16 23:52:24 +0700

    move get price to rental class and add test case

--- rental_test.py
+++ rental_test.py
@@ -17,13 +17,20 @@ class RentalTest(unittest.TestCase):
 		self.assertEqual("CitizenFour", m.get_title())
 		self.assertEqual(Movie.REGULAR, m.get_price_code())
 
-	@unittest.skip("TODO add this test when you refactor rental price")
+	# @unittest.skip("TODO add this test when you refactor rental price")
 	def test_rental_price(self):
 		rental = Rental(self.new_movie, 1)
 		self.assertEqual(rental.get_price(), 3.0)
 		rental = Rental(self.new_movie, 5)
 		self.assertEqual(rental.get_price(), 15.0)
-		self.fail("TODO add more tests for other movie categories")
+		rental = Rental(self.regular_movie, 3)
+		self.assertEqual(rental.get_price(), 3.5)
+		rental = Rental(self.regular_movie, 2)
+		self.assertEqual(rental.get_price(), 2.0)
+		rental = Rental(self.childrens_movie, 4)
+		self.assertEqual(rental.get_price(), 3.0)
+		rental = Rental(self.childrens_movie, 3)
+		self.assertEqual(rental.get_price(), 1.5)
 
 	@unittest.skip("TODO add test of frequent renter points when you add it to Rental")
 	def test_rental_points(self):
